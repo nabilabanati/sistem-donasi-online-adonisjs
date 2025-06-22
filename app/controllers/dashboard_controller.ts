2aef51d02640191aeeb781ff3f62d1c140d0284f
nabilabanati <nabilabanati126@gmail.com>
2025-06-22 14:50:40 +0700

    Fix dashboard

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -1,7 +1,23 @@
+import Donasi from '#models/donasi'
+import Donatur from '#models/donatur'
+import Kampanye from '#models/kampanye'
+import Kategori from '#models/kategori'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
   public async index({ view }: HttpContext) {
-    return view.render('pages/dashboard')
+    const kategori = await Kategori.query().count('* as total').pojo<{ total: number }>()
+    const totalKategori = kategori[0].total
+
+    const kampanye = await Kampanye.query().count('* as total').pojo<{ total: number }>()
+    const totalKampanye = kampanye[0].total
+
+    const donatur = await Donatur.query().count('* as total').pojo<{ total: number }>()
+    const totalDonatur = donatur[0].total
+
+    const donasi = await Donasi.query().count('* as total').pojo<{ total: number }>()
+    const totalDonasi = donasi[0].total
+
+    return view.render('pages/dashboard', { totalKategori, totalKampanye, totalDonatur, totalDonasi })
   }
 }
